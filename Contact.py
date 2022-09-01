085ee76e89146071b1bb29ecee9b3a44946fc3f4
thor1602 <thorbendhaenenstd@gmail.com>
2022-09-01 18:29:43 +0900

    added admin with credentials and verification methods + web design

--- Contact.py
+++ Contact.py
@@ -1,13 +1,13 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, validators, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email
-
-
-class contactForm(FlaskForm):
-    name = StringField(label='Name', validators=[DataRequired()])
-    email = StringField(label='Email', validators=[
-        DataRequired(), Email(granular_message=True)])
-    message = StringField(label='Message')
-
-
-submit = SubmitField(label="Log In")
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, validators, PasswordField, SubmitField
+# from wtforms.validators import DataRequired, Email
+#
+#
+# class contactForm(FlaskForm):
+#     name = StringField(label='Name', validators=[DataRequired()])
+#     email = StringField(label='Email', validators=[
+#         DataRequired(), Email(granular_message=True)])
+#     message = StringField(label='Message')
+#
+#
+# submit = SubmitField(label="Log In")

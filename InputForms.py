9e374a19502ced3a0b43bab770e6401b26f25455
thor1602 <thorbendhaenenstd@gmail.com>
2023-01-10 13:41:23 +0900

    updated security

--- InputForms.py
+++ InputForms.py
@@ -1,18 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import *
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, InputRequired
 
 class LoginForm(FlaskForm):
-    email = StringField(label='Email', validators=[DataRequired(), Email(granular_message=True)])
-    password = PasswordField(label='Password')
+    email = StringField(label='Email', validators=[InputRequired(), Email(granular_message=True)])
+    password = PasswordField(label='Password',validators=[InputRequired()])
     remember_me = BooleanField(label='Remember Me')
     submit = SubmitField(label="Log In")
 
 class ContactForm(FlaskForm):
-    email = StringField(validators=[DataRequired()])
-    name = StringField(validators=[DataRequired()])
+    email = StringField(validators=[InputRequired()])
+    name = StringField(validators=[InputRequired()])
     address = StringField()
-    phone = TelField(validators=[DataRequired()])
-    subject = StringField(validators=[DataRequired()])
-    message = TextAreaField(validators=[DataRequired()])
+    phone = TelField(validators=[InputRequired()])
+    subject = StringField(validators=[InputRequired()])
+    message = TextAreaField(validators=[InputRequired()])
     submit = SubmitField()
